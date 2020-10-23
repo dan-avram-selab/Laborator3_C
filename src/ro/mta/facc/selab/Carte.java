8f9dfb3d52bc89cd988ba19629335b39125d9866
dan-avram-selab <dan.avram@mta.ro>
2020-10-23 09:10:17 +0300

    Adaugare Utilizator, modificare Carte
    Detalii..
    
    Signed-off-by: dan-avram-selab <dan.avram@mta.ro>

--- src/ro/mta/facc/selab/Carte.java
+++ src/ro/mta/facc/selab/Carte.java
@@ -3,6 +3,8 @@ package ro.mta.facc.selab;
 public class Carte implements Comparable<Carte> {
     private String titlu;
     private String autor;
+    private int anPublicatie;
+    private int nrPagini;
 
     @Override
     public int compareTo(Carte o) {
@@ -13,18 +15,16 @@ public class Carte implements Comparable<Carte> {
         return 0;
     }
 
-    private int anAparitie;
-    private int nrPagini;
 
-    public Carte(String titlu, String autor, int anAparitie, int nrPagini) {
+    public Carte(String titlu, String autor, int anPublicatie, int nrPagini) {
         this.titlu = titlu;
         this.autor = autor;
-        this.anAparitie = anAparitie;
+        this.anPublicatie = anPublicatie;
         this.nrPagini = nrPagini;
     }
 
     @Override
     public String toString() {
-        return this.titlu + " " + this.autor + " " + ", publicată în anul " + this.anAparitie;
+        return this.titlu + " " + this.autor + " " + ", publicată în anul " + this.anPublicatie;
     }
 }
---
+++ src/ro/mta/facc/selab/Utilizator.java
@@ -0,0 +1,12 @@
+package ro.mta.facc.selab;
+
+import java.util.ArrayList;
+
+public class Utilizator {
+    private String nume;
+    private ArrayList<Carte> carti;
+
+    public Utilizator(String nume) {
+        this.nume = nume;
+    }
+}
